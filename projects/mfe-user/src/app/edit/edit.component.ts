8e043276407dc336b91ddf4e9bee618a73d53b43
Santiago Ramos <35112126+santironhacker@users.noreply.github.com>
2024-12-19 14:49:09 +0700

    [master] Add edit and details component content.

--- projects/mfe-user/src/app/edit/edit.component.ts
+++ projects/mfe-user/src/app/edit/edit.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthLibService } from 'auth-lib';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
 export class EditComponent {
+  authLibService = inject(AuthLibService);
+  userEmail = signal<string>(this.authLibService.user() || '');
 
+  handleOnClick(): void {
+    if (this.userEmail()?.trim().length > 3) {
+      this.authLibService.login(this.userEmail());
+    }
+  }
 }
